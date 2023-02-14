1c08e9e239e89c644401563af68f4ffcf5be1341
Van Neves <vaneves@vaneves.com>
2023-02-13 22:54:14 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(res => res.json())
+      .then(res => {
+        setCharacters(res.results);
+      });
+  });
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [character, setCharacter] = useState();
+
+  const show = (char) => {
+    setIsOpen(true);
+    setCharacter(char);
+  }
+
+  const hide = () => {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className='characters'>
+        {characters.map((character, index) => {
+          return <li key={index} onClick={() => show(character)}>
+            <img src={character.image} />
+            <p>{character.name}</p>
+          </li>
+        })}
+      </ul>
+      {isOpen && (
+        <div className='modal'>
+          <div>
+            {character.name}
+            <span className='close' onClick={hide}>X</span>
+          </div>
+          <p>Status: {character.status}</p>
+          <p>Origem: {character.origin.name}</p>
+        </div>
+      )}
     </div>
   );
 }
